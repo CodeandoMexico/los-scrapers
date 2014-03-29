70a342513c325263128884da0dc5d57a34269226
Milton Mazzarri <milmazz@gmail.com>
2014-03-29 13:29:53 -0600

    Simplificada la distribución de la app.

--- script/inegi/setup.py
+++ script/inegi/setup.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except:
-    from .distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     setup_requires=['d2to1'],
     d2to1=True,
     install_requires=[
         "requests",
         "pymongo"
-    ],
-    entry_points={
-        'console_scripts': [
-            'inegi_get_data = inegi.inegi_get_data:get_data',
-            'inegi_nosql = inegi.inegi_nosql:main',
-            'inegi_sql = inegi.inegi_sql:main'
-        ]
-    }
+    ]
 )
